189d70183a7cdd7ed2d406e0db1146330a1393fc
Artic <main@artic42.com>
2025-05-29 11:09:01 +0200

    Change space to leader in some mappings and add comments

--- nvim/lua/vimOptions.lua
+++ nvim/lua/vimOptions.lua
@@ -1,8 +1,13 @@
+-- Tabulation management
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+-- Line number management
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
+-- General mappings
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
