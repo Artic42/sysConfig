8e9654ba11c360ced29f0d77eea947a0b51745f0
Artic <main@artic42.com>
2025-06-01 21:01:40 +0200

    Add layzgit plugin

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -5,7 +5,7 @@ return {
     config = function ()
         vim.o.background = "dark" -- or "light" for light mode
         vim.cmd([[colorscheme gruvbox]])
-        vim.keymap.set('n' ,'<Leader>kl', ':set background=light<CR>')
-        vim.keymap.set('n' ,'<Leader>kd', ':set background=dark<CR>')
+        vim.keymap.set('n' ,'<Leader>kl', '<cmd>set background=light<CR>')
+        vim.keymap.set('n' ,'<Leader>kd', '<cmd>set background=dark<CR>')
     end
 }
