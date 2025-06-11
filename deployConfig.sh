fcb82d8829a32af2ab8c9b401c84f748b470b1fa
Artic <main@artic42.com>
2025-06-11 17:01:13 +0200

    Remove old deployment script

--- deployConfig.sh
+++
@@ -1,7 +0,0 @@
-# Neovim config change
-rm -rf ${HOME}/.config/nvim
-cp -r nvim/ ${HOME}/.config/nvim
-
-# Nushell config change
-rm -rf ${HOME}/.config/nushell
-cp -r nushell/ ${HOME}/.config/nushell
