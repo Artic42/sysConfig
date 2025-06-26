1318074f25c48c131cad0898e7cd744fa7b3ac82
Artic <main@artic42.com>
2025-06-26 14:36:47 +0200

    Change pylsp for pyright

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -3,34 +3,33 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function ()
+        config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "pylsp",
+                    "pyright",
                     "dockerls",
                     "docker_compose_language_service",
                     "bashls",
                     "nginx_language_server",
                     "yamlls",
                     "clangd",
-                }
+                },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
-        config = function ()
+        config = function()
             local capabalities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({capabalities = capabalities})
-            lspconfig.pylsp.setup({capabalities = capabalities})
+            lspconfig.lua_ls.setup({ capabalities = capabalities })
+            lspconfig.pyright.setup({ capabalities = capabalities })
             require("lspKeymaps")
         end,
-    }
-
+    },
 }
